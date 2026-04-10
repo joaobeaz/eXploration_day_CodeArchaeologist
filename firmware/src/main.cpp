fec910dfece564bd67399d68669bb559395dc135
jpbzx <1211189@isep.ipp>
2026-04-10 20:08:44 +0100

    update firmware libs usage

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,1 +1,22 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
+
+
+
+// Built-in LED pin on ESP32-DevKit
+#define LED_PIN 2
+
+void setup() {
+    // Initialize the LED pin as an output
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+    // Turn LED on
+    digitalWrite(LED_PIN, HIGH);
+    delay(1000);  // Wait 1 second
+    
+    // Turn LED off
+    digitalWrite(LED_PIN, LOW);
+    delay(1000);  // Wait 1 second
+}
